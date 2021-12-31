7b4b4cfa3fe23469c08321017bc73e1a00e22663
Ariq Khoiri <chiisanayumee@gmail.com>
2021-12-31 17:26:16 +0700

    kawaishop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Annonuncement from '../components/Annonuncement'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Catgories from '../components/Categories'
+import Products from '../components/Products'
+import Newsletters from '../components/Newsletters'
+import Footer from '../components/Footer'
+
+
+export const Home = () => {
+    return (
+        <div>
+            <Annonuncement/>
+            <Navbar/>
+            <Slider/>
+            <Catgories/>
+            <Products/>
+            <Newsletters/>
+            <Footer/>
+        </div>
+    )
+}
